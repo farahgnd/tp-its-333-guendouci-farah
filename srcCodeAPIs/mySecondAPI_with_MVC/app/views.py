266843ac3c6a532fba8f55ab1735726cb1ad4b2f
Farah <farahgnd@gmail.com>
2026-01-12 18:01:35 +0100

    remise de l'exo 2 et 3

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -3,8 +3,23 @@ from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
 
-### EXO2 - API with simple display
 
-### EXO3 - API with parameters display 
+
+### EXO2 - API with simple display
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
 
 ### EXO4 - API with parameters retrieved from URL 
+
+@app.route('/', methods=['GET'])
+def salutation_url():
+    nom = request.args.get("nom")  # récupère le paramètre "nom" dans l'URL
+    if not nom:
+        return jsonify({"error": "Aucun nom fourni"}), 400
+    return jsonify({"message": f"Bonjour {nom}"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
